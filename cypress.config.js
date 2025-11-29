513bb0a4bca2eeeea61d4cbd015b13d68ac06f41
Lucas Ramalho <lucasramalho.nasci@gmail.com>
2025-11-29 19:22:51 -0300

    Mochawesome + geracao de reporter

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,16 @@ module.exports = defineConfig({
     baseUrl: "https://www.saucedemo.com",
     specPattern: "cypress/e2e/specs/**/*.spec.js",
     supportFile: "cypress/support/e2e.js",
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
   },
+},
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "reports/mocha",
+    charts: true,
+    overwrite: false,
+    html: false,
+    json: true
+  }
 });
