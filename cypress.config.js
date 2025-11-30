3a5dcfb860eb422318769eabe1dc0d0a290bae5c
Lucas Ramalho <lucasramalho.nasci@gmail.com>
2025-11-29 21:28:14 -0300

    ajuste ci para baixar explicitamente o mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "reports/mocha",
+    overwrite: false,
+    html: false,
+    json: true,
+    charts: true
+  },
   e2e: {
     baseUrl: "https://www.saucedemo.com",
     specPattern: "cypress/e2e/specs/**/*.spec.js",
     supportFile: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
-  },
-},
-  reporter: "cypress-mochawesome-reporter",
-  reporterOptions: {
-    reportDir: "reports/mocha",
-    charts: true,
-    overwrite: false,
-    html: false,
-    json: true
+      require("cypress-mochawesome-reporter/plugin")(on, config);
+      return config;
+    }
   }
 });
